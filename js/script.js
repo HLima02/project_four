36cd46670ef26c69a944cabf5be72bc617b262f6
Hiago Lima <hiago.nascimento@enext.com.br>
2020-01-23 18:57:58 -0200

    correcao menu da pagina

--- js/script.js
+++ js/script.js
@@ -1,10 +1,28 @@
 function abrirMenu(){
 	var botaoAbrir = document.getElementById('menu_list')
 	botaoAbrir.style.display = "block"
-	document.getElementById('botao_fechar').style.display = 'block'
+	if (botaoAbrir.style.display = "block"){
+		document.getElementById('botao_fechar').style.display = 'block'
+	}
+	else{
+		document.getElementById('botao_fechar').style.display = 'none'
+	}
 }
 
 function fecharMenu(){
 	var botaoFechar = document.getElementById("menu_list")
 	botaoFechar.style.display = "none"
+}
+
+
+function larguraPagina(){
+	var largura = window.innerWidth
+	console.log(largura)
+	if(largura > 665){
+		document.getElementById('menu_list').style.display = 'block'
+	}
+	else{
+		document.getElementById('menu_list').style.display = 'none'
+		document.getElementById('botao_fechar').style.display = 'none'
+	}
 }
