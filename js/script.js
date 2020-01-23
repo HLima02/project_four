7bbbefb79e47d50db4e0b292a8beb3952e974fcf
Hiago Lima <hiago.nascimento@enext.com.br>
2020-01-23 18:05:43 -0200

    Versão final do site

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function abrirMenu(){
+	var botaoAbrir = document.getElementById('menu_list')
+	botaoAbrir.style.display = "block"
+}
+
+function fecharMenu(){
+	var botaoFechar = document.getElementById("menu_list")
+	botaoFechar.style.display = "none"
+}
